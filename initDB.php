cc2aab4d296fe14e26dda7e1b7be0c05ef6f3f65
drs64 <drs64@njit.edu>
2019-09-19 12:33:36 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -3,7 +3,7 @@
 ini_set('display_errors',1);
 ini_set('display_startuo_errors', 1);
 
-error_reporting(E_All);
+//error_reporting(E_All);
 
 require('config.php');
 echo $host;
@@ -23,15 +23,28 @@ try{
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
 	$stmt = $db->prepare($query);
 	$r = $stmt->execute();
-	echo "<br>" . $r . "<br>";
-        $user = "DharmikSoni";
-        $pin = 2832;
-        //DB Insert query
-        //Bind values
-        $r = $stmt->execute(TRUE);        
-        $insert_query = "INSERT INTO `TestUsers`(`username`, `pin`) VALUES (':username',;pin)";
-        $stmt = $db->prepare($insert_query);
-        $r = $stmt->execute($insert_query);
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table")  . "<br>";
+        unset($r);
+
+	$insert_query = "INSERT INTO `TestUsers`( `username`, `pin`) 
+		VALUES (:username, :pin)";
+	$stmt = $db->prepare($insert_query);
+	$newUser = "Billy";
+	$newPin = 1234;
+	$r = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
+	
+	print_r($stmt->errorInfo());
+	
+	//TODO catch error from DB
+	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
+	
+	
+        $select_query = "select * from `TestUsers` where username = :username";
+	$stmt = $db->prepare($select_query);
+	$r = $stmt->execute(array(":username"=> "Billy"));
+	$results = $stmt->fetch(PDO::FETCH_ASSOC);
+	($stmt->errorInfo());
+	echo "<pre>" . var_export($results, true) . "</pre>"; 
 
 		
 }
