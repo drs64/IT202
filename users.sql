a5213095bb5902272fa5f69ecf828f65300896b9
drs64 <54951504+drs64@users.noreply.github.com>
2019-11-01 11:16:37 -0400

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  
+  `password` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `users` (`id`, `username`,  `password`) VALUES
+(1, 'Dharmik',  '1234'),
+(1, 'jay',  '9876')
+
