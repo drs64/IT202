c687e1bb000f2530211a10eb2e077d45e894ecaa
drs64 <54951504+drs64@users.noreply.github.com>
2019-11-01 11:17:09 -0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -2,7 +2,6 @@
 CREATE TABLE `users` (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(80) NOT NULL,
-  
   `password` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
